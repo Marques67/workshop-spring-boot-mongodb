b2584e75f441453367405e6b48759323024e2f57
Lucas Marques <lucasmarquesff@hotmail.com>
2021-09-01 15:14:38 -0300

    Projecting author data with DTO

---
+++ src/main/java/br/com/workshopspring/Workshop_Spring/dto/AuthorDTO.java
@@ -0,0 +1,34 @@
+package br.com.workshopspring.Workshop_Spring.dto;
+
+import br.com.workshopspring.Workshop_Spring.domain.User;
+
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+
+    private String id;
+    private String name;
+
+    public AuthorDTO() {}
+
+    public AuthorDTO(User obj) {
+        id = obj.getId();
+        name = obj.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
